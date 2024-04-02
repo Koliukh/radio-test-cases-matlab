244e24b0463f35291bd166f8785bd54cb1fa96aa
Dmitrii Koliukh <33293118+Koliukh@users.noreply.github.com>
2024-04-02 22:02:39 +0300

    Update waveformAnalyzerSimulationScenario.m

--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,12 @@
 % Скрипт для запуска waveformAnalyxer
+clc
+clear
+addpath quadriga_src/
+addpath waveform/
+
+
+waveformanalyzerObject1 = WaveformAnalyzer();
+waveformanalyzerObject1.plotPowerSpectrumDensity();
+waveformanalyzerObject1.plotPayloadConstellation();
+waveformanalyzerObject1.calcWaveformParameters();
+waveformanalyzerObject1.calcEvmPerformance();
